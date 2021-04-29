b880a4fa07a71272b1272216f39323cc15b0efff
Tihomir_K <dronaldo12345@gmail.com>
2021-04-29 11:43:04 +0300

    added function to update state of the player widget

--- src/view/CellWidget.java
+++ src/view/CellWidget.java
@@ -4,14 +4,23 @@ import view.helpers.GlobalStyles;
 import view.helpers.RoundedBorder;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CellWidget extends JButton {
+public class CellWidget extends JButton implements ActionListener {
+
     public CellWidget() {
         //super(text);
+
         setContentAreaFilled(false);
         setFocusPainted(false);
         setBorder(new RoundedBorder(5));
         setOpaque(true);
         setBackground(GlobalStyles.PRIMARY_COLOR);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
