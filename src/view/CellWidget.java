2eb19ea6369d87aa4728e625d60ba38feba5aa8c
Tihomir_K <dronaldo12345@gmail.com>
2021-04-29 15:19:44 +0300

    improved borders in components, added title to start menu

--- src/view/CellWidget.java
+++ src/view/CellWidget.java
@@ -1,5 +1,6 @@
 package view;
 
+import view.helpers.CustomActionButton;
 import view.helpers.GlobalStyles;
 import view.helpers.RoundedBorder;
 
@@ -7,16 +8,10 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CellWidget extends JButton implements ActionListener {
+public class CellWidget extends CustomActionButton implements ActionListener {
 
     public CellWidget() {
-        //super(text);
-
-        setContentAreaFilled(false);
-        setFocusPainted(false);
-        setBorder(new RoundedBorder(5));
-        setOpaque(true);
-        setBackground(GlobalStyles.PRIMARY_COLOR);
+        super("");
     }
 
     @Override
