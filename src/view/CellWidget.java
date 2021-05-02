fa5b2e888dd5ba2546a0b2070ae3c55264769076
Tihomir_K <dronaldo12345@gmail.com>
2021-05-02 19:16:35 +0300

    1_6_construct_gui added PlayerActionListener

--- src/view/CellWidget.java
+++ src/view/CellWidget.java
@@ -1,21 +1,42 @@
 package view;
 
+import model.Cell;
 import view.helpers.CustomActionButton;
-import view.helpers.GlobalStyles;
-import view.helpers.RoundedBorder;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.util.Objects;
 
-public class CellWidget extends CustomActionButton implements ActionListener {
+public class CellWidget extends CustomActionButton {
 
-    public CellWidget() {
+    private final int _row;
+    private final int _col;
+
+    public CellWidget(int row, int col, int fieldSize, Character initLetter) {
         super("");
+        if (row < 0 || col < 0) throw new IllegalArgumentException("Invalid indexes");
+        _row = row;
+        _col = col;
+        int fontSize;
+        switch (fieldSize) {
+            case 5 -> fontSize = 60;
+            case 6 -> fontSize = 55;
+            case 7 -> fontSize = 45;
+            case 8 -> fontSize = 35;
+            default -> fontSize = 30;
+        }
+        setFont(new Font("Roboto", Font.PLAIN, fontSize));
+        this.setLetter(initLetter);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void setSelection(Cell.SelectionState state) {
+        switch (state) {
+            case NOT_SELECTED -> setBackground(Color.WHITE);
+            case SELECTED_TO_WRITE_LETTER -> setBackground(Color.decode("#D0D0D0"));
+            case SELECTED_TO_CREATE_WORD -> setBackground(Color.decode("#B8B8B8"));
+        }
+    }
 
+    public void setLetter(Character letter) {
+        setText(Objects.requireNonNull(letter).toString());
     }
 }
